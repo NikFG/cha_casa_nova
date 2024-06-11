8898bad20847e61e1953e542e7ab0d24ed859d7f
Nikollas <nikollasferreirag@gmail.com>
2024-06-11 19:50:55 -0300

    corrige gridView para responsivo

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -5,6 +5,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'dart:js' as js;
 
+import 'package:responsive_grid/responsive_grid.dart';
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -34,10 +36,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    final double height = size.height / 2;
+    final double itemWidth = size.width / 2;
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            toolbarHeight: height / 2,
             title: Text(widget.title),
+            flexibleSpace: Image.network(
+              'https://cdn.pixabay.com/photo/2016/07/07/16/46/dice-1502706_640.jpg',
+              fit: BoxFit.fitWidth,
+            ),
+            centerTitle: true,
             leading: IconButton(
               icon: const Icon(Icons.arrow_back),
               onPressed: () {
@@ -59,25 +70,37 @@ class _MyHomePageState extends State<MyHomePage> {
             ]),
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: GridView.builder(
-                itemCount: produtos.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  childAspectRatio: 0.7,
-                  mainAxisSpacing: 20,
-                  crossAxisSpacing: 16,
-                ),
-                itemBuilder: (context, index) {
-                  Produto produto = produtos[index];
-                  return InkWell(
-                    onTap: () {
-                      js.context.callMethod('open', [produto.link]);
-                    },
-                    child: ProductCard(produto: produto),
-                  );
-                }),
+            padding:
+                const EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.2),
+            child: ResponsiveGridList(
+                desiredItemWidth: itemWidth > 600 ? 500 : 200,
+                minSpacing: 50,
+                children: produtos
+                    .map(
+                      (p) => InkWell(
+                          onTap: () {
+                            js.context.callMethod('open', [p.link]);
+                          },
+                          child: ProductCard(produto: p, width: itemWidth)),
+                    )
+                    .toList()),
           ),
+          // child: GridView.builder(
+          //     itemCount: produtos.length,
+          //     gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          //       maxCrossAxisExtent: itemWidth,
+          //       crossAxisSpacing: 20,
+          //       childAspectRatio: 0.7,
+          //     ),
+          //     itemBuilder: (context, index) {
+          //       Produto produto = produtos[index];
+          //       return InkWell(
+          //         onTap: () {
+          //           js.context.callMethod('open', [produto.link]);
+          //         },
+          //         child: ProductCard(produto: produto),
+          //       );
+          //     }),
         ));
   }
 
--- lib/product_card.dart
+++ lib/product_card.dart
@@ -6,13 +6,12 @@ import 'Produto.dart';
 class ProductCard extends StatelessWidget {
   const ProductCard({
     Key? key,
-    this.width = 140,
-    this.aspectRetio = 1.02,
     required this.produto,
+    required this.width,
   }) : super(key: key);
 
-  final double width, aspectRetio;
   final Produto produto;
+  final double width;
 
   @override
   Widget build(BuildContext context) {
@@ -21,62 +20,57 @@ class ProductCard extends StatelessWidget {
     FirebaseFirestore db = FirebaseFirestore.instance;
 
     return SizedBox(
-      width: width,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AspectRatio(
-            aspectRatio: 1.02,
-            child: Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: kSecondaryColor.withOpacity(0.1),
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Image.network(
-                produto.imagem,
-                width: 50,
-                height: 50,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: kSecondaryColor.withOpacity(0.1),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Image.network(
+            produto.imagem,
+            width: width,
+            height: 250,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          produto.nome,
+          style: Theme.of(context).textTheme.bodyMedium,
+          maxLines: 2,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              formatNumber(produto.preco),
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w600,
+                color: kPrimaryColor,
               ),
             ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            produto.nome,
-            style: Theme.of(context).textTheme.bodyMedium,
-            maxLines: 2,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                formatNumber(produto.preco),
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w600,
-                  color: kPrimaryColor,
-                ),
+            IconButton(
+              onPressed: () async {
+                db
+                    .collection("produtos")
+                    .doc(produto.id)
+                    .update({"comprado": !produto.comprado});
+              },
+              padding: const EdgeInsets.all(2),
+              icon: Icon(
+                produto.comprado
+                    ? Icons.favorite
+                    : Icons.favorite_border_outlined,
+                color: Colors.red,
               ),
-              IconButton(
-                onPressed: () async {
-                  db
-                      .collection("produtos")
-                      .doc(produto.id)
-                      .update({"comprado": !produto.comprado});
-                },
-                padding: const EdgeInsets.all(2),
-                icon: Icon(
-                  produto.comprado
-                      ? Icons.favorite
-                      : Icons.favorite_border_outlined,
-                  color: Colors.red,
-                ),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+            ),
+          ],
+        )
+      ],
+    ),);
   }
 
   String formatNumber(double number) {
